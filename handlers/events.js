2c661d24b75907c0ed7932abec8a8b38960a2a2a
Soldier380 <75277509+Soldier380@users.noreply.github.com>
2023-10-20 10:43:41 -0500

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const chalk = require('chalk')
+
 module.exports = (client) => {
     fs.readdirSync('./events/').filter((file) => file.endsWith('.js')).forEach((event) => {
       	require(`../events/${event}`);
